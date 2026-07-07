8eabf48272f36c73eb262fbd89c1d4955650164e
Ruslan Bulimov <imrusbully@gmail.com>
2026-07-07 11:47:50 +0300

    first commit:

---
+++ src/main/java/com/demo/common/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.demo.common.constants;
+
+public class Constants {
+
+    public final static String API_V1_PATH = "/api/v1";
+
+    public final static String ME_PATH = "/me";
+
+    public final static String ADMIN_PATH = "/admin";
+
+    public final static String AUTH_PATH = "/auth";
+
+    public final static String PUBLIC_PATH = "/public";
+
+    public final static String SESSIONS_PATH = "/sessions";
+
+}
